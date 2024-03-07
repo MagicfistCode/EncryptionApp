d5aa84b617b4bcc3b75dd061aa13d48fe7d26b7b
Dzaky <117564615+MagicfistCode@users.noreply.github.com>
2024-03-08 05:02:04 +0800

    Add project files.

---
+++ EncryptionApp/AESEncryption.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.Bot.Configuration.Encryption;
+
+namespace EncryptionApp
+{
+    internal class AESEncryption
+    {
+        public static byte[] AESEncrypt(String text, byte[] key, byte[] iv) 
+        {
+            byte[] cipheredtext;
+            using (Aes aes = Aes.Create())
+            {
+                ICryptoTransform encrpytor = aes.CreateEncryptor(key, iv);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encrpytor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream)) {
+                        streamWriter.Write(text);}
+                    }
+                    cipheredtext = memoryStream.ToArray();
+                }
+            }
+
+                return cipheredtext;
+        }
+
+        public static string AESDecrypt(byte[] cipheredText, byte[] key, byte[] iv) 
+        {
+            string text = String.Empty;
+            using (Aes aes = Aes.Create())
+            {
+                ICryptoTransform AESDecryptor = aes.CreateDecryptor(key, iv);
+                using (MemoryStream memoryStream = new MemoryStream(cipheredText)) 
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, AESDecryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            text = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return text;
+        }
+
+    }
+}
---
+++ EncryptionApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Microsoft.Bot.Configuration.Encryption;
+
+namespace EncryptionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring the message variable to encrypt
+            string message = "";
+
+            //declaring the variable for user's choice of encryption type
+            string choice;
+
+            byte[] key = new byte[16];
+            byte[] iv = new byte[16];
+
+            RandomNumberGenerator.Create().GetBytes(key);
+            RandomNumberGenerator.Create().GetBytes(iv);
+
+            //asking the user to enter a message
+            Console.Write("Enter a message you want to encrypt: ");
+            //assigning the user input as the message
+            message = Console.ReadLine();
+
+            //asking the user the encyption algorithm they want
+            Console.WriteLine("Choose your prefered encryption algorithm (enter 1, 2, 3, or 4): \n" +
+                "1) AES \n" +
+                "2) RSA \n" +
+                "3) DES \n" +
+                "4) 3DES");
+
+            //user input is assigned to choice
+            choice = Console.ReadLine();
+
+            //validation that user's input is either 1, 2, 3, or 4
+            while(!choice.Equals("1") && !choice.Equals("2") && !choice.Equals("3") && !choice.Equals("4")) 
+            {
+                Console.WriteLine("That is not a valid option, please choose again...");
+                choice = Console.ReadLine();
+            }
+
+            //user chooses AES encrption type
+            if (choice.Equals("1"))
+            {
+                //encrpyting the message
+                byte[] encryptedMessage = AESEncryption.AESEncrypt(message, key, iv);
+                string encryptedMessageString = Convert.ToBase64String(encryptedMessage);
+                Console.WriteLine("Encrypted Message: " + encryptedMessageString);
+
+                //decrypting the message
+                string decryptedMessage = AESEncryption.AESDecrypt(encryptedMessage, key, iv);
+                Console.WriteLine("Decrypted Message: " + decryptedMessage);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
