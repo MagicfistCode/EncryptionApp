8509993dae26608cf8c4c4dccb202ef174d1f4d9
Dzaky <117564615+MagicfistCode@users.noreply.github.com>
2024-03-08 23:18:41 +0800

    creating RSA Encrpytion Class

---
+++ EncryptionApp/RSAEncryption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionApp
+{
+    internal class RSAEncryption
+    {
+        private static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RSAEncryption()
+        {
+            _privateKey = rsa.ExportParameters(true);
+            _publicKey = rsa.ExportParameters(false);
+        }
+    }
+}
